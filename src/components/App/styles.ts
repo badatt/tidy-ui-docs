3a0d657323dce777ce72a0109445f065d1d36ca4
badatt <dev.badatt@gmail.com>
2023-08-01 21:17:56 +0530

    chore: added manifest updates

--- src/components/App/styles.ts
+++ src/components/App/styles.ts
@@ -2,6 +2,11 @@ import { createGlobalStyle } from 'styled-components';
 import { color, hsla, ITheme } from '@tidy-ui/commons';
 
 export const GlobalStyle = createGlobalStyle<ITheme>`
+
+  h1, h2, h3, h4, h5, h6 {
+    scroll-margin-top: 80px;
+  }
+
   /* width */
   ::-webkit-scrollbar {
     width: 0.5rem;
