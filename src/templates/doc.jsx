05af8de439792379b543f66a4153dae401c8ff08
Balu Praveen Datty <dev.badatt@gmail.com>
2023-08-20 22:25:41 +0530

    chore: lint

--- src/templates/doc.jsx
+++ src/templates/doc.jsx
@@ -21,7 +21,7 @@ const Doc = ({ children, ...data }) => {
         <Text.h2 bld margin="2rem 0">
           {title}
         </Text.h2>
-        <Text.base style={{lineHeight: 1.8}}>{description}</Text.base>
+        <Text.base style={{ lineHeight: 1.8 }}>{description}</Text.base>
         <MDXProvider components={mdxComponents}>{children}</MDXProvider>
       </ContentWrapper>
     </MdDoc>
